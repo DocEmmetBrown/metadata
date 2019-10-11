229eacda69a18bc44c6b4e2ba99f79265bd91179
Guillaume Winter <gwinter@heroku.com>
2019-10-11 09:56:48 +0200

    cleanup code

--- fetch_metadata.rb
+++ fetch_metadata.rb
@@ -33,68 +33,58 @@ def result(previous, url)
     # includes the result
     $result_data[to_url(previous)] = url.last if previous
     $get_urls.delete(url)
+    return
   end
-  
+  r = fetch_url(uri)
+  $get_urls.delete(url)
+  if r.nil?
+    p "r is nil #{url}" if ENV["DEBUG"]
+    return
+  end
 
-  if uri
-    if url.last == "user-data"
-      r = fetch_url(uri)
-      $get_urls.delete(url)
-      if r.nil?
-        p "r is nil #{url}" if ENV["DEBUG"]
-      else
-        $result_data[to_url(url)] = r.body
-      end
-      return
-    end
-    r = fetch_url(uri)
+  while r.code == "301"
+    r = fetch_url(URI(r["location"]))
     if r.nil?
-      p "r is nil #{url}" if ENV["DEBUG"]
-      $get_urls.delete(url)
+      p "r is nil #{url} after 301" if ENV["DEBUG"]
       return
     end
-    while r.code == "301"
-      r = fetch_url(URI(r["location"]))
-      if r.nil?
-        p "r is nil #{url} after 301" if ENV["DEBUG"]
-        $get_urls.delete(url)
-        return
-      end
-    end
-    if r.code != "200"
-      # if there is an error, it's also probably that
-      # the previous request was the result
-      $result_data[to_url(previous)] = url.last if previous
-      $get_urls.delete(url)
-    else
-      $get_urls.delete(url)
-      # trying to add a fake new part for the url
-      fake_url = url.dup << "xxxx"
-      r2 = fetch_url(URI(to_url(fake_url)))
-      if r2.nil?
-        p "r2 is nil #{url}" if ENV["DEBUG"]
-        return
-      end
-      # if the result is the same, we're at the end of
-      # the fetching
-      if r.body == r2.body
-        $result_data[to_url(url)] = r.body
-      else
-        # if it's a json block
-        # append it as a whole
-        if r.body[0] == "{"
-          p "json block #{r.body}" if ENV["DEBUG"]
-          $result_data[to_url(url)] = r.body
-        else
-          # otherwise, add all the findings for
-          # the new loop
-          r.body.split("\n").each do |s|
-            s.chop! if s[-1] == "/"
-            new_url = url.dup << s
-            $get_urls[new_url] = url
-          end
-        end
-      end
+  end
+  if r.code != "200"
+    # if there is an error, it's also probably that
+    # the previous request was the result
+    p "r.code = #{r.code} #{url}" if ENV["DEBUG"]
+    $result_data[to_url(previous)] = url.last if previous
+    return
+  end
+
+  # trying to add a fake new part for the url
+  fake_url = url.dup << "xxxx"
+  r2 = fetch_url(URI(to_url(fake_url)))
+  if r2.nil?
+    p "r2 is nil #{url}" if ENV["DEBUG"]
+    return
+  end
+  # if the result is the same, we're at the end of
+  # the fetching
+  if r.body == r2.body
+    p "r = r2 #{url}" if ENV["DEBUG"]
+    $result_data[to_url(url)] = r.body
+    return
+  end
+  # if it's a json block or a userdata
+  # append it as a whole
+  # TODO : find a better diagnostic for
+  # multilines output
+  if r.body[0] == "{" || url.last == "user-data"
+    p "block #{r.body}" if ENV["DEBUG"]
+    $result_data[to_url(url)] = r.body
+  else
+    # otherwise, add all the findings for
+    # the new loop
+    r.body.split("\n").each do |s|
+      s.chop! if s[-1] == "/"
+      new_url = url.dup << s
+      $get_urls[new_url] = url
     end
   end
 end
@@ -110,7 +100,7 @@ new_result = {}
 # only for cleanup
 p $result_data if ENV["DEBUG"]
 
-$result_data.each do |k, v|
+$result_data.sort_by{|k,v| k}.to_h.each do |k, v|
   new_k = k.gsub(endpoint.join("/") + "/", "")
   begin
     new_v = JSON.parse(v)
